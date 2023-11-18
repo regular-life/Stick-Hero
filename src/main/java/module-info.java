54c45e82dbd2ed31fe404cbcaa3999ce2a9bb11e
regular-life <yash22586@iiitd.ac.in>
2023-11-19 00:36:21 +0530

    start

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.project.stickhero {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.project.stickhero to javafx.fxml;
+    exports com.project.stickhero;
+}
