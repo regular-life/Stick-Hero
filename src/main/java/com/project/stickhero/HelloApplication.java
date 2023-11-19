58e0285773702eba6d8f77f57b7ea5db313eca94
NakulGarg-IIITD <nakul22309@iiitd.ac.in>
2023-11-19 22:13:38 +0530

    d

--- src/main/java/com/project/stickhero/HelloApplication.java
+++ src/main/java/com/project/stickhero/HelloApplication.java
@@ -11,12 +11,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Stick Hero");
         stage.setScene(scene);
         stage.show();
+        stage.setResizable(false);
     }
-
     public static void main(String[] args) {
         launch();
     }
